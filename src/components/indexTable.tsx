fead5ba650d271b9cb43c5856c3b215a4ef6d28f
hukurouo <owlbook248@gmail.com>
2021-06-06 19:48:27 +0900

    total

--- src/components/indexTable.tsx
+++ src/components/indexTable.tsx
@@ -2,7 +2,26 @@ import { Table, Thead, Tbody, Tr, Th, Td, Box, Text } from "@chakra-ui/react";
 import Link from "next/link";
 
 export const IndexTable = (props: { result: any[] }) => (
+  <>
   <Box overflowX={{base:"auto", sm:"auto", md: "auto"}}>
+  <Table size="sm" mt={8}>
+    <Thead>
+      <Tr>
+        <Th></Th>
+        <Th>単勝1点</Th>
+        <Th>単勝2点</Th>
+        <Th>複勝1点</Th>
+        <Th>ワイド3頭BOX</Th>
+        <Th>ワイド5頭BOX</Th>
+        <Th>三連複5頭BOX</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      {totalNum(props.result)}
+    </Tbody>
+  </Table>
+  </Box>
+  <Box overflowX={{base:"auto", sm:"auto", md: "auto"}} pt={8}>
   <Table size="sm" mt={8}>
     <Thead>
       <Tr>
@@ -24,7 +43,50 @@ export const IndexTable = (props: { result: any[] }) => (
     </Tbody>
   </Table>
   </Box>
+  </>
 );
+
+const totalNum = (result: any) => {
+  const count = result.length * 1000
+  const totalTan = result.map((item: any)=>{return item.tan}).reduce((prev: number, current:number)=>{return prev+current;})
+  const totalTan2 = result.map((item: any)=>{return item.tan2}).reduce((prev: number, current:number)=>{return prev+current;})
+  const totalHuku = result.map((item: any)=>{return item.huku}).reduce((prev: number, current:number)=>{return prev+current;})
+  const totalWide3box = result.map((item: any)=>{return item.wide3box}).reduce((prev: number, current:number)=>{return prev+current;})
+  const totalWide5box = result.map((item: any)=>{return item.wide5box}).reduce((prev: number, current:number)=>{return prev+current;})
+  const total3huku5box = result.map((item: any)=>{return item["3huku5box"]}).reduce((prev: number, current:number)=>{return prev+current;})
+
+  const recTan = String(Math.round(((count + totalTan) / count)*10000)/100) + "%"
+  const recTan2 = String(Math.round(((count + totalTan2) / count)*10000)/100) + "%"
+  const recHuku = String(Math.round(((count + totalHuku) / count)*10000)/100) + "%"
+  const recWide3box = String(Math.round(((count + totalWide3box) / count)*10000)/100) + "%"
+  const recWide5box = String(Math.round(((count + totalWide5box) / count)*10000)/100) + "%"
+  const rec3huku5box = String(Math.round(((count + total3huku5box) / count)*10000)/100) + "%"
+
+  return (
+    <>
+    <Tr key={0}>
+      <Td minW={24}><Text fontWeight="semibold">合計</Text></Td>
+      <Td minW={24}>{colorNum(totalTan)}</Td>
+      <Td minW={24}>{colorNum(totalTan2)}</Td>
+      <Td minW={24}>{colorNum(totalHuku)}</Td>
+      <Td minW={24}>{colorNum(totalWide3box)}</Td>
+      <Td minW={24}>{colorNum(totalWide5box)}</Td>
+      <Td minW={24}>{colorNum(total3huku5box)}</Td>
+
+    </Tr>
+    <Tr key={0}>
+      <Td minW={24}><Text fontWeight="semibold">回収率</Text></Td>
+      <Td minW={24}>{recTan}</Td>
+      <Td minW={24}>{recTan2}</Td>
+      <Td minW={24}>{recHuku}</Td>
+      <Td minW={24}>{recWide3box}</Td>
+      <Td minW={24}>{recWide5box}</Td>
+      <Td minW={24}>{rec3huku5box}</Td>
+
+    </Tr>
+    </>
+  );
+}
 
 const displayThColor = (index: number, item: any) => {
   return (
